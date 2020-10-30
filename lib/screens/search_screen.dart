a5e823f8332b1f73b81c60c2b12e9ea70e0f97c8
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-10-30 19:50:30 +0530

    Connectivity checks added

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:convert';
 
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:youtube_clone/models/Post.dart';
@@ -17,11 +19,13 @@ class _SearchScreenState extends State<SearchScreen> {
   String searchQuery = '';
   TextEditingController dataController = TextEditingController();
   Post post;
+  ConnectivityResult connectivityResult;
 
   @override
   void initState() {
     super.initState();
     dataController.addListener(() => getData());
+    checkConnectivityStatus();
   }
 
   @override
@@ -30,14 +34,24 @@ class _SearchScreenState extends State<SearchScreen> {
     dataController.dispose();
   }
 
+  checkConnectivityStatus() async {
+    connectivityResult = await (Connectivity().checkConnectivity());
+  }
+
   Future<void> getData() async {
-    var res = await http.get(
-      "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=$searchQuery&type=video&key=$APIkey",
-    );
-    var jsonData = jsonDecode(res.body);
-    setState(() {
-      post = Post.fromJson(jsonData);
-    });
+    checkConnectivityStatus();
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
+      var res = await http.get(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=$searchQuery&type=video&key=$APIkey",
+      );
+      var jsonData = jsonDecode(res.body);
+      if (mounted) {
+        setState(() {
+          post = Post.fromJson(jsonData);
+        });
+      }
+    }
   }
 
   @override
@@ -53,52 +67,93 @@ class _SearchScreenState extends State<SearchScreen> {
           padding: const EdgeInsets.all(5.0),
           child: Builder(
             builder: (context) => TextField(
-              controller: dataController,
-              autofocus: true,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-              ),
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 20,
-              ),
-              onSubmitted: (data) {
-                setState(() {
-                  searchQuery = data;
-                });
-              },
-              onChanged: (value) {
-                setState(() {
-                  searchQuery = value;
-                });
-              },
-            ),
+                controller: dataController,
+                autofocus: true,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                ),
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+                onSubmitted: (data) {
+                  if (mounted) {
+                    setState(() {
+                      searchQuery = data;
+                    });
+                  }
+                },
+                onChanged: (value) {
+                  setState(() {
+                    searchQuery = value;
+                  });
+                }),
           ),
         ),
         actions: [
           IconButton(
             icon: Icon(Icons.search),
             onPressed: () {
-              setState(() {
-                searchQuery = dataController.text;
-              });
+              if (mounted) {
+                setState(() {
+                  searchQuery = dataController.text;
+                });
+              }
             },
           )
         ],
       ),
       body: searchQuery.length > 3
-          ? ListView.builder(
-              itemCount: post.items.length,
-              itemBuilder: (context, index) => SearchVideoCard(
-                channelName: "${post.items[index].snippet.channelTitle}",
-                thumbnail: "${post.items[index].snippet.thumbnails.high.url}",
-                title: "${post.items[index].snippet.title}",
-                uploadTime: "${post.items[index].snippet.publishTime}",
-                description: "${post.items[index].snippet.description}",
-                videoId: "${post.items[index].id.videoId}",
+          ? connectivityResult == ConnectivityResult.none
+              ? Card(
+                  elevation: 4.0,
+                  margin: EdgeInsets.all(10.0),
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10.0),
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    width: double.infinity,
+                    child: Text(
+                      'Internet connection not available',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ),
+                )
+              : ListView.builder(
+                  itemCount: post.items.length,
+                  itemBuilder: (context, index) => SearchVideoCard(
+                    channelName: "${post.items[index].snippet.channelTitle}",
+                    thumbnail:
+                        "${post.items[index].snippet.thumbnails.high.url}",
+                    title: "${post.items[index].snippet.title}",
+                    uploadTime: "${post.items[index].snippet.publishTime}",
+                    description: "${post.items[index].snippet.description}",
+                    videoId: "${post.items[index].id.videoId}",
+                  ),
+                )
+          : Container(
+              //margin: EdgeInsets.only(left: 20.0, right: 20.0),
+              height: MediaQuery.of(context).size.height * 0.1,
+              margin: EdgeInsets.only(left: 30.0),
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/logos/YouTube-logo.png',
+                    height: 50.0,
+                    width: 50.0,
+                  ),
+                  SizedBox(
+                    width: 7.0,
+                  ),
+                  Text('Type to search YouTube'),
+                ],
               ),
-            )
-          : Container(),
+            ),
     );
   }
 }
